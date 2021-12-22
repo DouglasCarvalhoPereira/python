6d43a80869c90c88ddc27f5564d5ae5483fc1cd5
Douglas Carvalho <carvalhodouglaspereira@gmail.com>
2021-12-21 23:42:16 -0300

    Adicionada classe Cartao de Credito.

--- oop_modulos_e_mais/contabanco.py
+++ oop_modulos_e_mais/contabanco.py
@@ -2,51 +2,77 @@
 from datetime import datetime, timezone
 
 class ContaCorrente():
-    
+    """
+    Cria um objeto conta corrente para gerencia as contas correntes.
+
+    Attributos:
+        nome: (str) Nome do Cliente
+        cpf: (str) Cpf do Cliente com pontos e traço
+        Agencia: (int) Agencia responsável pela conta do cliente.
+        num_conta: (int) Número da conta corrente do cliente
+        saldo: (float) Saldo atual da conta do cliente
+        limite: (float) Limite de cheque especial
+        tansacoes: (list) Extrato de transações da conta do cliente
+
+    """
     @staticmethod
     def _data_hora():
+        """
+        Pega data e hora atual no formato adequado.
+        """
         hora = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         return hora
 
     def __init__(self, nome, cpf, agencia, num_conta) -> None:
-        self.nome = nome
-        self.cpf = cpf
-        self.limite = None
-        self.saldo = 0
-        self.agencia = agencia
-        self.conta = num_conta
-        self.transacoes = []
-
+        self._nome = nome
+        self._cpf = cpf
+        self._limite = None
+        self._saldo = 0
+        self._agencia = agencia
+        self._num_conta = num_conta
+        self._transacoes = []
+        self._cartoes = []
 
     def consultar_saldo(self):
-        return f"Saldo: R$ {self.saldo}"
+        return f"_saldo: R$ {self._saldo}"
 
     def depositar(self, valor_deposito):
-        self.saldo += valor_deposito
-        self.transacoes.append((valor_deposito, self.saldo, ContaCorrente._data_hora()))
+        self._saldo += valor_deposito
+        self._transacoes.append((valor_deposito, self._saldo, ContaCorrente._data_hora()))
 
     def _limite_conta(self):
-        self.limite =  -1000
-        return self.limite
+        self._limite =  -1000
+        return self._limite
 
     def sacar(self, valor_sacado):
-        if  self.saldo - valor_sacado < self._limite_conta():
+        if  self._saldo - valor_sacado < self._limite_conta():
             print('Você não tem aldo suficiente para sacar esse valor.')
             self.consultar_saldo()
         else:
-            self.saldo -= valor_sacado
-            self.transacoes.append((valor_sacado, self.saldo, ContaCorrente._data_hora()))
+            self._saldo -= valor_sacado
+            self._transacoes.append((valor_sacado, self._saldo, ContaCorrente._data_hora()))
 
     def transferir(self, valor, conta_destino):
-        self.saldo -= valor
-        self.transacoes.append((valor, self.saldo, ContaCorrente._data_hora()))
-        conta_destino.saldo += valor
-        conta_destino.transacoes.append((valor, self.saldo, ContaCorrente._data_hora()))       
+        self._saldo -= valor
+        self._transacoes.append((valor, self._saldo, ContaCorrente._data_hora()))
+        conta_destino._saldo += valor
+        conta_destino._transacoes.append((valor, self._saldo, ContaCorrente._data_hora()))       
     
     def consultar_limite_chequeespecial(self):
         print(f'Seu limite de Cheque especial é de R$ {self._limite_conta():.2f}')        
 
     def extrato_bancario(self):
         print('EXTRATO BANCÁRIO')
-        for transacao in self.transacoes:
+        for transacao in self._transacoes:
             print(transacao)
+
+
+class CartaoCredito:
+
+    def __init__(self, titular, conta_corrente) -> None:
+        self.numero = 1234
+        self.titular = titular
+        self.validade = None
+        self.cod_seguranca = None
+        self.conta_corrente = conta_corrente
+        conta_corrente._cartoes.append(self)
--- oop_modulos_e_mais/system.py
+++ oop_modulos_e_mais/system.py
@@ -1,4 +1,4 @@
-from contabanco import ContaCorrente
+from contabanco import CartaoCredito, ContaCorrente
 
 cliente_1 = ContaCorrente('Douglas', '23.345.567-99', 1020, 2344)
 cliente_2 = ContaCorrente('Clayton', '26.305.561-30', 1021, 2342)
@@ -12,3 +12,6 @@ cliente_1.extrato_bancario()
 cliente_1.transferir(1000, cliente_2)
 print(cliente_2.consultar_saldo())
 cliente_2.extrato_bancario()
+
+cartao_douglas = CartaoCredito('Douglas', cliente_1)
+print(cartao_douglas.conta_corrente._cartoes[0].numero)
